b87f69ac5cd55d0238438891ec155ab3a07a5e09
BiplawSingh <biplawgaurav@gmail.com>
2023-12-14 18:36:00 +0530

    Added password comaprison function

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 const UserRepository = require('../repository/user-repository');
 
@@ -35,6 +36,15 @@ class UserService {
             return response;
         } catch(error) {
             console.log("Something went wrong in token validation");
+            throw error;
+        }
+    }
+
+    checkPassword(userInputPlainPassword, encryptedPassword) {
+        try {
+            return bcrypt.compareSync(userInputPlainPassword, encryptedPassword);
+        } catch(error) {
+            console.log("Something went wrong in password comparison");
             throw error;
         }
     }
