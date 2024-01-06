6e1eb60c5cb98ee40f5697f608368a3e078a9ddf
BiplawSingh <biplawgaurav@gmail.com>
2024-01-06 13:52:21 +0530

    Added validation error class

---
+++ src/utils/error-handler.js
@@ -0,0 +1,18 @@
+
+
+class AppErrors extends Error {
+    constructor(
+        name = 'AppError',
+        message  ='Something went wrong',
+        explanation = 'Something went wrong',
+        statusCode = statusCode.INTERNAL_SERVER_ERROR
+    ) {
+        super();
+        this.message = message,
+        this.explanation = explanation,
+        this.name = name,
+        this.statusCode = statusCode
+    }
+}
+
+module.exports = AppErrors;
